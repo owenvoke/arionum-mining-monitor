b11c54393a8b78fdde81ceabeea10f3f9958067f
pxgamer <owzie123@gmail.com>
2018-11-21 17:28:26 +0000

    Add WorkerDiscovery model

---
+++ app/WorkerDiscovery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class WorkerDiscovery
+ *
+ * @property int    $worker
+ * @property Carbon $date
+ * @property int    $difficulty
+ * @property int    $dl
+ * @property string $nonce
+ * @property string $argon
+ * @property int    $retries
+ * @property int    $confirmed
+ */
+class WorkerDiscovery extends Model
+{
+    /** @var string */
+    protected $table = 'worker_discovery';
+
+    /** @var bool */
+    public $timestamps = false;
+
+    /** @var array */
+    protected $dates = [
+        'date',
+    ];
+}
