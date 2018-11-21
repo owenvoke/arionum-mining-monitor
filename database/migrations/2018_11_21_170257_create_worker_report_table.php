8e8ecdf3ce584e98fa79b23fede7c710d50d4de0
pxgamer <owzie123@gmail.com>
2018-11-21 17:09:14 +0000

    Add worker_report table migration

---
+++ database/migrations/2018_11_21_170257_create_worker_report_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateWorkerReportTable
+ */
+class CreateWorkerReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('worker_report', function (Blueprint $table) {
+            $table->unsignedInteger('worker');
+            $table->date('date');
+            $table->integer('hashes');
+            $table->integer('elapsed');
+            $table->decimal('rate', 20, 3);
+
+            $table->foreign('worker')->references('id')->on('workers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('worker_report');
+    }
+}
