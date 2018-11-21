978faec48c439851a66feaae99f9244474711365
pxgamer <owzie123@gmail.com>
2018-11-21 17:22:28 +0000

    Add Worker model

---
+++ app/Worker.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Worker
+ *
+ * @property int    $id
+ * @property string $name
+ * @property string $ip
+ * @property Carbon $date
+ * @property string $type
+ */
+class Worker extends Model
+{
+    /** @var bool */
+    public $timestamps = false;
+
+    /** @var array */
+    protected $dates = [
+        'date',
+    ];
+}
