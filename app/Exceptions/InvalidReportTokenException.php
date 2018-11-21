9faf6e3e89661c4d0c067d193456b53e75566d34
pxgamer <owzie123@gmail.com>
2018-11-21 22:50:36 +0000

    Add middleware for report token validation

---
+++ app/Exceptions/InvalidReportTokenException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * Class InvalidReportTokenException
+ */
+class InvalidReportTokenException extends Exception
+{
+    //
+}
---
+++ app/Http/Middleware/VerifyReportToken.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\InvalidReportTokenException;
+use Closure;
+
+class VerifyReportToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @return mixed
+     * @throws InvalidReportTokenException
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->get('token') !== config('arionum.report.token')) {
+            throw new InvalidReportTokenException('unauthorized');
+        }
+
+        return $next($request);
+    }
+}
