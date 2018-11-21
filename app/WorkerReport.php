bfc557f74ed547664dad1df14f7762cbcc471533
pxgamer <owzie123@gmail.com>
2018-11-21 17:25:39 +0000

    Add WorkerReport model

---
+++ app/WorkerReport.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class WorkerReport
+ *
+ * @property int    $worker
+ * @property Carbon $date
+ * @property int    $hashes
+ * @property int    $elapsed
+ * @property float  $rate
+ */
+class WorkerReport extends Model
+{
+    /** @var bool */
+    public $timestamps = false;
+
+    /** @var array */
+    protected $dates = [
+        'date',
+    ];
+}
