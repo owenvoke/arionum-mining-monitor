4938b8624a9182f0fd8b02f457ff1597f68ba539
pxgamer <owzie123@gmail.com>
2018-11-29 16:25:15 +0000

    Update to shorten PHPDoc comments in User

--- app/User.php
+++ app/User.php
@@ -23,22 +23,14 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $hidden = [
         'password',
         'remember_token',
