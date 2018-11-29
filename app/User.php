bc004cec1bb5b0d9cc0f78517d393ba66cec505a
pxgamer <owzie123@gmail.com>
2018-11-29 16:24:25 +0000

    Add PHPDoc type hints to User model

--- app/User.php
+++ app/User.php
@@ -2,11 +2,22 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 /**
  * Class User
+ *
+ * @property int         $id
+ * @property string      $name
+ * @property string      $email
+ * @property Carbon|null $email_verified_at
+ * @property string      $password
+ * @property string      $remember_token
+ * @property string      $report_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  */
 class User extends Authenticatable
 {
