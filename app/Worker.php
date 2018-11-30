f91fc2e802d44ba1135a5fcffe0e9df436a1b995
pxgamer <owzie123@gmail.com>
2018-11-30 11:15:50 +0000

    Update models to match database

--- app/Worker.php
+++ app/Worker.php
@@ -8,29 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Class Worker
  *
- * @property int    $id
- * @property int    $user_id
- * @property string $name
- * @property string $ip
- * @property Carbon $date
- * @property string $type
+ * @property int         $id
+ * @property int         $user_id
+ * @property string      $name
+ * @property string      $ip
+ * @property string      $type
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  */
 class Worker extends Model
 {
-    /** @var bool */
-    public $timestamps = false;
-
-    /** @var array */
-    protected $dates = [
-        'date',
-    ];
-
     /** @var array */
     protected $fillable = [
         'user_id',
         'name',
         'ip',
-        'date',
         'type',
     ];
 }
